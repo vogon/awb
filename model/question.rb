86a74c9c452b85f9543b4ee381326784aabf4179
Bill Abney <bill.abney@gmail.com>
2012-03-23 09:47:52 -0700

    Question class should be responsible for knowing what to do with different types. Also maps are cool.

--- model/question.rb
+++ model/question.rb
@@ -1,7 +1,14 @@
 class Question
-	def initialize(text, arity)
-		@text = text
-		@arity = arity
+	def initialize(question)
+	  if question.is_a? String
+		  @text = question
+		  @arity = 1
+		elsif question.is_a? Hash
+		  @text = question[:question]
+		  @arity = question[:arity]
+	  else
+	    fail TypeError.new("Questions should be a String or Hash!")
+	  end
 	end
 
 	attr_reader :text, :arity
