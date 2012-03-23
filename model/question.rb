fc0992d09b2b72545ba87e71c1f1e14a558f9954
Bill Abney <bill.abney@gmail.com>
2012-03-23 09:51:37 -0700

    need to raise something here -- is ArgumentError the right choice?

--- model/question.rb
+++ model/question.rb
@@ -6,6 +6,7 @@ class Question
 		elsif question.is_a? Hash
 		  @text = question[:question]
 		  @arity = question[:arity]
+		  fail ArgumentError.new("Need a question and arity keys in the question hash!") unless @text && @arity
 	  else
 	    fail TypeError.new("Questions should be a String or Hash!")
 	  end
