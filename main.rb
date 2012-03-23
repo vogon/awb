6293ed676f18eba91fa4796d0b71d1ca2f1f193f
Bill Abney <bill.abney@gmail.com>
2012-03-23 12:15:55 -0700

    let's respond to / just so I know right away if this sucker's running

--- main.rb
+++ main.rb
@@ -1,3 +1,7 @@
 # request handlers
 require 'newgame'
 require 'join'
+
+get '/' do
+  'And the Academy Award for [not giving a shit about the third world] goes to [all you can eat shrimp for 4.99]'
+end
